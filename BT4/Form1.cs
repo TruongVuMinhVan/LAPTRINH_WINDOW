d5ca8efe9fd2defdb38033e9105e54145504afd2
TruongVuMinhVan <133574385+TruongVuMinhVan@users.noreply.github.com>
2024-09-27 22:07:04 +0700

    Add files via upload

---
+++ BT4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DS_2form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.Rows.Add (frm.MaNV, frm.TenNV, frm.LuongCB);
+            }
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn mục để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0) // Kiểm tra có mục nào được chọn không
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                // Cập nhật thông tin từ các TextBox
+               Form2 frm = new Form2();
+                if (frm.ShowDialog() == DialogResult.OK)
+                {
+                    selectedRow.Cells["col_MaNV"].Value = frm.MaNV;
+                    selectedRow.Cells["col_TenNV"].Value = frm.TenNV;
+                    selectedRow.Cells["col_Luong"].Value = frm.LuongCB;
+
+                    frm.ClearFields();
+                }
+            }
+            else { MessageBox.Show("Vui lòng chọn một nhân viên để sửa.");}
+        }
+
+        private void btn_Dong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BT4/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DS_2form
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public string MaNV { get; set; }
+        public string TenNV { get; set; }
+        public int LuongCB { get; set; }
+
+        
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Yes_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_maNV.Text) || string.IsNullOrEmpty(txt_Ten.Text) || string.IsNullOrEmpty(txt_Luong.Text))
+            {
+                MessageBox.Show("Vui Lòng Nhập Đủ Thông Tin!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MaNV = txt_maNV.Text;
+                TenNV = txt_Ten.Text;
+                LuongCB = int.Parse(txt_Luong.Text);
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        public void ClearFields()
+        {
+            txt_maNV.Clear();
+            txt_Ten.Clear();
+            txt_Luong.Clear();
+        }
+
+        private void btn_Skip_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
